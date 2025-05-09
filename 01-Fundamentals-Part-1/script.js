388584b3098cd4086be6510e9993a2d53d7539cf
ChrisChen332 <155408584+ChrisChen332@users.noreply.github.com>
2025-05-08 23:55:37 -0400

    May8

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,2 +1,25 @@
-let js = "amazing";
-console.log("djawidhjia");
+// let js = "amazing";
+// console.log("djawidhjia");
+
+// const birthYear = 1991;
+// // birthYear = 1990;
+
+// // const job;
+
+// var job = "programmer";
+// job = "teacher;";
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+const firstName = "Chris";
+const lastName = "Chen";
+console.log(firstName + " " + lastName);
+
+let x = 10 + 5;
+x += 10;
+console.log(x);
