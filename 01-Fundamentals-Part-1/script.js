80a787d9067455412d812860f48d854764191d06
ChrisChen332 <155408584+ChrisChen332@users.noreply.github.com>
2025-06-07 02:01:10 -0400

    June7

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -9,17 +9,55 @@
 // var job = "programmer";
 // job = "teacher;";
 
-const now = 2037;
-const ageJonas = now - 1991;
-const ageSarah = now - 2018;
-console.log(ageJonas, ageSarah);
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
 
-console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
 
-const firstName = "Chris";
-const lastName = "Chen";
-console.log(firstName + " " + lastName);
+// const firstName = "Chris";
+// const lastName = "Chen";
+// console.log(firstName + " " + lastName);
 
-let x = 10 + 5;
-x += 10;
-console.log(x);
+// let x = 10 + 5;
+// x += 10;
+// console.log(x);
+
+// console.log(ageJonas > ageSarah);
+// const isFullAge = ageSarah >= 18;
+
+// const firstName = "Jonas";
+// const job = "teacher";
+// const birthYear = 1991;
+// const year = 2037;
+// const jonas = `I'm ${firstName},  ${year - birthYear} years old ${job}`;
+// console.log(jonas);
+
+// const age = 15;
+// const isAd = age >= 18;
+
+// if (isAd) {
+//   console.log("🚃🚗");
+// } else {
+//   const yearsLeft = 18 - age;
+//   console.log(`Years left: ${yearsLeft}`);
+// }
+
+// const birthYear = 1998;
+// let century;
+// if (birthYear <= 2000) {
+//   century = 20;
+// } else {
+//   century = 2021;
+// }
+// console.log(century);
+// const inputYear = "1991";
+// console.log(Number(inputYear) + 18);
+
+const money = 0;
+if (money) {
+  console.log("not poor");
+} else {
+  console.log("poor");
+}
