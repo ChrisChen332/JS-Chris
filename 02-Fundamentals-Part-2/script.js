f9b728f72f58e07ea2ef106d678365bcc9a408a8
ChrisChen332 <155408584+ChrisChen332@users.noreply.github.com>
2025-06-13 01:21:56 -0400

    June 13

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -40,9 +40,57 @@
 // console.log(calcTip(bills[0]))
 // const totals = [bills[0] + tips[0],bills[1] + tips[1], bills[2] + tips[2] ]
 
-const chris = {
-  firstName: "chris",
-  lastName: "chen",
-  age: 20,
-  job: "student",
+// const chris = {
+//   firstName: "chris",
+//   lastName: "chen",
+//   birthYear: 1991,
+//   job: "student",
+//   friends: ["Micheal", "Jack", "ryan"],
+//   hasDriversLicense: true,
+//   calcAge: function () {
+//     this.age = 2037 - this.birthYear;
+//   },
+// };
+
+// // console.log(chris.firstName);
+// // console.log(chris["firstName"]);
+
+// // console.log(
+// //   `${chris.firstName} has ${chris.friends.length} friends, and his best friend is called ${chris.friends[0]}`
+// // );
+// chris.calcAge();
+
+// console.log(chris.age);
+
+const mark = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.bmi = (this.mass / this.height) * this.height;
+    return this.bmi;
+  },
 };
+
+const john = {
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.bmi = (this.mass / this.height) * this.height;
+    return this.bmi;
+  },
+};
+mark.calcBMI();
+john.calcBMI();
+if (john.bmi > mark.bmi) {
+  console.log(
+    `${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})!`
+  );
+} else if (john.bmi < mark.bmi) {
+  console.log(
+    `${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})!`
+  );
+} else {
+  console.log("Equal");
+}
