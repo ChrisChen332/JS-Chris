5f47a16ea17e442e16836c4ab53455d599796bf0
ChrisChen332 <155408584+ChrisChen332@users.noreply.github.com>
2025-06-15 23:26:03 -0400

    Jan15

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -1,3 +1,9 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
+function printForecast(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(`${arr[i]}C in ${i + 1} days`);
+  }
+}
 
+printForecast([17, 21, 23]);
