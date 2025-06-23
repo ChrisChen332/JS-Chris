c96eecb21404a4a7b26bce3a8f12b210e526ece5
ChrisChen332 <155408584+ChrisChen332@users.noreply.github.com>
2025-06-22 21:31:05 -0400

    june22

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -1,1 +1,9 @@
 'use strict';
+//Selecting Element
+const score0EI = document.querySelector('#score--0');
+const score1EI = document.getElementById('score--1');
+const diceEI = document.querySelector('.dice');
+
+score0EI.textContent = 0;
+score1EI.textContent = 0;
+diceEI.classList.add('hidden');
