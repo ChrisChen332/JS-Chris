d922a13d90e565c4b1b40c6795f8a160a9499b01
ChrisChen332 <155408584+ChrisChen332@users.noreply.github.com>
2025-08-21 22:45:33 -0400

    Aug21

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -10,7 +10,32 @@ function createBooking(
     numPassengers,
     price,
   };
-  console.log(booking);
   bookings.push(booking);
 }
 createBooking('LH');
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+const greet = greeting => {
+  return name => {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+greet('hey')('chris');
