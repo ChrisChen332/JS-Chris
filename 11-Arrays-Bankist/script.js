c829e308e3363070b651882125c5b9ac500f3a8f
ChrisChen332 <155408584+ChrisChen332@users.noreply.github.com>
2025-09-16 12:44:15 -0400

    Update script.js

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -74,3 +74,7 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+const arr = ['a', 'b', 'c', 'd'];
+arr.splice(2, 1);
+console.log(arr);
